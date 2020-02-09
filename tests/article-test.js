066582bc28757773c4bc274f732536f4e6871898
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-09 22:40:54 +0000

    Updates article-test

--- tests/article-test.js
+++ tests/article-test.js
@@ -1,6 +1,6 @@
 
 (function ArticleHasHeadlineAndId() {
-  var article = new Article("Storm Ciara - on the move")
+  var article = new Article("Storm Ciara - on the move", 1)
   assert.isTrue(article.viewHeadline() === "Storm Ciara - on the move");
-  assert.isTrue(article.id === 0)
+  assert.isTrue(article.id === 1)
 })();
