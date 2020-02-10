c40a2b08b81f8ddfff91cb9a1970bcf1ef2c733f
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-10 00:07:42 +0000

    Updates article list and test

--- src/article-list.js
+++ src/article-list.js
@@ -5,12 +5,14 @@
     this.articles = []
   }
 
-  ArticleList.prototype.addArticle = function(headline) {
+  ArticleList.prototype.addArticle = function(headline, id, url) {
     var id = this.articles.length
-    var article = new Article(headline, id)
+    var article = new Article(headline, id, url)
     this.articles.push(article)
     return article;
   }
+
+ 
 
   exports.ArticleList = ArticleList;
 })(this)
--- tests/article-list-test.js
+++ tests/article-list-test.js
@@ -1,15 +1,23 @@
 (function ArticleListTakesArticles() {
     var articleList = new ArticleList();
-    articleList.addArticle("Corona DeathS")
-    articlelist.addArticle("Oscars 2020")
-    assert.isTrue(articlelist.articles[0].headline === "Corona Deaths");
-    assert.isTrue(articlelist.articles[0].id === 0);
-    assert.isTrue(articlelist.articles[1].headline === "Oscars 2020");
-    assert.isTrue(articlelist.articles[1].id === 1);
+    articleList.addArticle("Corona Deaths", 1, "url")
+    articleList.addArticle("Oscars 2020", 2, "url2")
+    assert.isTrue(articleList.articles[0].headline === "Corona Deaths");
+    assert.isTrue(articleList.articles[0].id === 0);
+    assert.isTrue(articleList.articles[0].url === "url");
+    assert.isTrue(articleList.articles[1].headline === "Oscars 2020");
+    assert.isTrue(articleList.articles[1].id === 1);
+    assert.isTrue(articleList.articles[1].url === "url2");
 })();
 
 (function AddIdToArticle() {
     var articleList = new ArticleList();
-    articleList.addArticle("Corona Deaths")
+    articleList.addArticle("Corona Deaths", 0, "url")
     assert.isTrue(articleList.articles[0].id === 0);
+})();
+
+(function AddURLToArticle() {
+    var articleList = new ArticleList();
+    articleList.addArticle("Corona Deaths", 0, "url")
+    assert.isTrue(articleList.articles[0].url === "url");
 })();
