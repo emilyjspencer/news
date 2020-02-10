0aac598d0679209d27dfc0784eb0f7be1837af5d
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-10 00:14:24 +0000

    Updates article-list model

--- src/article-list.js
+++ src/article-list.js
@@ -3,13 +3,16 @@
 (function(exports) {
   function ArticleList() {
     this.articles = []
+    this.idCount = 0
   }
 
   ArticleList.prototype.addArticle = function(headline, id, url) {
-    var id = this.articles.length
-    var article = new Article(headline, id, url)
-    this.articles.push(article)
-    return article;
+    //var id = this.articles.length
+    //var article = new Article(headline, id, url)
+    //this.articles.push(article)
+    //return article;
+    this.articles.push(new Article(headline = headline, id = this.idCount, url = url));
+    this.idCount++
   }
 
  
